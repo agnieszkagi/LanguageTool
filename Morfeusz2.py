c12108e5cbaeac631d6822a4f5767bedb08be18c
agnieszkagi <agn.ginalska@gmail.com>
2020-03-16 22:51:23 +0100

    def Decline_Noun()

--- Morfeusz2.py
+++ Morfeusz2.py
@@ -2,44 +2,46 @@
 import re
 import morfeusz2
 
+# def()
 
-
-
-def Decline(keyword, case):
+def Decline_Noun(keyword, case):
     ''' Decline nouns and adjectives '''
     morf = morfeusz2.Morfeusz()
     list_of_morphosyntactic_forms = morf.generate(keyword)
     if case == 'nom':
         for tuple in list_of_morphosyntactic_forms:
-            #element.replace("(", "").replace(")", "")
-            #list_of_forms = element.split(",")
             for element in tuple:
                 if "subst:sg:nom" in element:
                     result = tuple[0]
+    elif case == 'gen':
+        for tuple in list_of_morphosyntactic_forms:
+            for element in tuple:
+                if "subst:sg:gen" in element:
+                    result = tuple[0]
+    elif case == 'dat':
+        for tuple in list_of_morphosyntactic_forms:
+            for element in tuple:
+                if "subst:sg:dat" in element:
+                    result = tuple[0]
+    elif case == 'acc':
+        for tuple in list_of_morphosyntactic_forms:
+            for element in tuple:
+                if "subst:sg:nom.acc" in element:
+                    result = tuple[0]
+                elif "subst:sg:acc" in element:
+                    result = tuple[0]
+    elif case == 'inst':
+        for tuple in list_of_morphosyntactic_forms:
+            for element in tuple:
+                if "subst:sg:inst" in element:
+                    result = tuple[0]
+    elif case == 'loc':
+        for tuple in list_of_morphosyntactic_forms:
+            for element in tuple:
+                if "subst:sg:loc" in element:
+                    result = tuple[0]
     return result
 
-print(Decline("endoskop", "nom"))
+print(Decline_Noun("karczoch", "loc"))
+print(Decline_Noun("książka", "acc"))
 #dokończę
-
-"""
-def Decline(keyword, case):
-    ''' Decline nouns and adjectives '''
-    if case == 'nom':
-        endings = keyword.split("|")
-        result = endings[0] + endings[1]
-    elif case == 'gen':
-        endings = keyword.split("|")
-        result = endings[0] + endings[2]
-    elif case == 'dat':
-        endings = keyword.split("|")
-        result = endings[0] + endings[3]
-    elif case == 'acc':
-        endings = keyword.split("|")
-        result = endings[0] + endings[4]
-    elif case == 'inst':
-        endings = keyword.split("|")
-        result = endings[0] + endings[5]
-    elif case == 'loc':
-        endings = keyword.split("|")
-        result = endings[0] + endings[6]
-    return result"""
