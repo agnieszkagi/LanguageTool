f65f9c24220e04f2dbc5c01bb45abf0cb1239cdd
agnieszkagi <agn.ginalska@gmail.com>
2020-03-17 00:05:20 +0100

    Def DeleteTags

--- LanguageTool.py
+++ LanguageTool.py
@@ -1,28 +1,10 @@
 #! python3
 import re
-import morfeusz2
 
 glossary = ["<subst>endoskop||u|owi||em|ie</subst>", "<subst>teleskop||u|owi||em|ie</subst>", "<subst>książ|ka|ki|ce|kę|ką|ce</subst>" ]
 # Function decline nouns
-morf = morfeusz2.Morfeusz()
-
-
-"""
 def Decline(keyword, case):
     ''' Decline nouns and adjectives '''
-
-    morf = morfeusz2.Morfeusz()
-    list_of_morphosyntactic_interpretations = morf.generate(keyword)
-    for element in list_of_morphosyntactic_interpretations:
-        if case == 'nom':
-            list_of_forms = element.split(',')
-            for form in list_of_forms():
-                if 'subst:sg:nom' in form:
-                    result = list_of_forms[0]
-
-print(Decline("teleskop", "nom"))"""
-"""
-
     if case == 'nom':
         endings = keyword.split("|")
         result = endings[0] + endings[1]
@@ -50,4 +32,91 @@ print(Decline(glossary[2], 'acc'))
 print(Decline(glossary[2], 'inst'))
 print(Decline(glossary[2], 'loc'))
 
+def getIndexPositions(listOfElements, element):
+    ''' Returns the indexes of all occurrences of give element in
+    the list- listOfElements '''
+    indexPosList = []
+    indexPos = 0
+    while True:
+        try:
+            # Search for item in list from indexPos to the end of list
+            indexPos = listOfElements.index(element, indexPos)
+            # Add the index position in list
+            indexPosList.append(indexPos)
+            indexPos += 1
+        except ValueError as e:
+            break
+
+    return indexPosList
+
+# FINDING KEYWORDS IN THE TEXT
+
+text  ="""
+Zamontować cewnik na <subst>endoskop||u|owi||em|ie</subst>. Przygotować <subst>endoskop||u|owi||em|ie</subst>.
+Przysunąć przedmiot do <subst>teleskop||u|owi||em|ie</subst>.
+Położyć przy <subst>endoskop||u|owi||em|ie</subst> lub przed <subst>endoskop||u|owi||em|ie</subst> lub
+nad <subst>endoskop||u|owi||em|ie</subst> lub też pod <subst>endoskop||u|owi||em|ie</subst>.
+Bez <subst>endoskop||u|owi||em|ie</subst>. Przerwa.
+Trzymać daleko od <subst>endoskop||u|owi||em|ie</subst>.
+<subst>książ|ka|ki|ce|kę|ką|ce</subst> leżała na stole. Włożyłam zakładkę do <subst>książ|ka|ki|ce|kę|ką|ce</subst>.
 """
+Regex = re.compile(r'<subst>\S+</subst>') # dopisać, że może być kropka lub przecinek, ale nie musi?
+Regex2 = re.compile(r'<adj>\S+</adj>')
+list_of_subst = list(set(Regex.findall(text)))
+list_of_adj = list(set(Regex2.findall(text)))
+print(list_of_subst)
+print(len(list_of_subst))
+
+#add spaces before .,;: to separate the keywords
+text = text.replace(".", " .").replace(",", " ,").replace(";", " ;").replace(":", " :").replace("\n", " \n ") # todo cudzysłów, \n itd
+list_of_words = text.split(" ")
+
+print(list_of_words)
+# 1. NOUNS
+# indexing all NOUNS
+list_of_indexes =[]
+for element in list_of_subst:
+    indexes = getIndexPositions(list_of_words, element)
+    for index in indexes:
+        list_of_indexes.append(index)
+list_of_indexes.sort()
+print(list_of_indexes)
+
+# Lists of pronoms
+
+pronoms_gen = ["bez", "blisko", "dla", "do", "dookoła", "koło", "mimo", "naokoło", "obok", "od", "ode", "około",\
+               "oprócz", "prócz", "podczas", "podług", "pomimo", "poniżej", "pośrodku", "pośród", "powyżej", "spod",\
+               "spomiędzy", "sponad", "spośród", "spoza", "sprzed", "u", "według", "wobec", "wokoło", "wokół",\
+               "wskutek", "wśród", "wzdłuż", "zamiast", "znad", "zza", "naprzeciw", "naprzeciwko"]
+pronoms_dat = ["dzięki", "ku", "przeciw", "przeciw", "wbrew"]
+pronoms_acc = ["przez"]
+pronoms_inst = []
+pronoms_loc = ["przy"]
+# Decline words inside list:
+
+for index in list_of_indexes:
+    print(index)
+#genitif
+    if list_of_words[index - 1].lower() in pronoms_gen:
+        list_of_words[index] = Decline(list_of_words[index], "gen")
+#datif
+    elif list_of_words[index - 1].lower() in pronoms_dat:
+        list_of_words[index] = Decline(list_of_words[index], "dat")
+#accusatif
+    elif list_of_words[index - 1].lower() in pronoms_acc:
+        list_of_words[index] = Decline(list_of_words[index], "acc")
+#instrumental
+    elif list_of_words[index - 1].lower() in pronoms_inst:
+        list_of_words[index] = Decline(list_of_words[index], "inst")
+#locatif
+    elif list_of_words[index - 1].lower() in pronoms_loc:
+        list_of_words[index] = Decline(list_of_words[index], "loc")
+    else:
+        list_of_words[index] = Decline(list_of_words[index], "nom")
+
+print(list_of_words)
+
+#print(getIndexPositions(list_of_words, "<subst>endoskop||u|owi||em|ie</subst>"))
+
+
+#TODO assertion : assert element.split("|").len() = 6
--- Morfeusz2.py
+++ Morfeusz2.py
@@ -4,8 +4,10 @@ import morfeusz2
 
 # def()
 
+
 def Decline_Noun(keyword, case):
     ''' Decline nouns and adjectives '''
+    result = ""
     morf = morfeusz2.Morfeusz()
     list_of_morphosyntactic_forms = morf.generate(keyword)
     if case == 'nom':
@@ -42,6 +44,105 @@ def Decline_Noun(keyword, case):
                     result = tuple[0]
     return result
 
+#Test funkcji
+"""
 print(Decline_Noun("karczoch", "loc"))
 print(Decline_Noun("książka", "acc"))
-#dokończę
+"""
+
+def getIndexPositions(listOfElements, element):
+    ''' Returns the indexes of all occurrences of give element in
+    the list- listOfElements '''
+    indexPosList = []
+    indexPos = 0
+    while True:
+        try:
+            # Search for item in list from indexPos to the end of list
+            indexPos = listOfElements.index(element, indexPos)
+            # Add the index position in list
+            indexPosList.append(indexPos)
+            indexPos += 1
+        except ValueError as e:
+            break
+    return indexPosList
+
+
+def DeleteTags(keyword):
+    new_keyword = keyword.replace('<subst>', "").replace('</subst>', ""). replace('<adj>', "").replace('</adj>', "")
+    return new_keyword
+
+# FINDING KEYWORDS IN THE TEXT
+#TODO : Dodać tekst
+text  ="""
+Zamontować cewnik na <subst>endoskop</subst>. Przygotować <subst>endoskop</subst>.
+Przysunąć przedmiot do <subst>teleskop</subst>.
+Położyć przy <subst>endoskop</subst> lub przed <subst>endoskop</subst> lub
+nad <subst>endoskop</subst> lub też pod <subst>endoskop</subst>.
+Bez <subst>endoskop</subst>. Przerwa.
+Trzymać daleko od <subst>endoskop</subst>.
+<subst>książka</subst> leżała na stole. Włożyłam zakładkę do <subst>książka</subst>.
+"""
+
+#Regex
+
+Regex = re.compile(r'<subst>\S+</subst>') # dopisać, że może być kropka lub przecinek, ale nie musi?
+Regex2 = re.compile(r'<adj>\S+</adj>')
+list_of_subst = list(set(Regex.findall(text)))
+list_of_adj = list(set(Regex2.findall(text)))
+print(list_of_subst)
+print(len(list_of_subst))
+
+#add spaces before .,;: to separate the keywords
+text = text.replace(".", " .").replace(",", " ,").replace(";", " ;").replace(":", " :").replace("\n", " \n ") # todo cudzysłów, \n itd
+list_of_words = text.split(" ")
+
+print(list_of_words)
+# 1. NOUNS
+# indexing all NOUNS
+list_of_indexes =[]
+for element in list_of_subst:
+    indexes = getIndexPositions(list_of_words, element)
+    for index in indexes:
+        list_of_indexes.append(index)
+list_of_indexes.sort()
+print(list_of_indexes)
+
+# Pronoms
+pronoms_gen = ["bez", "blisko", "dla", "do", "dookoła", "koło", "mimo", "naokoło", "obok", "od", "ode", "około",\
+               "oprócz", "prócz", "podczas", "podług", "pomimo", "poniżej", "pośrodku", "pośród", "powyżej", "spod",\
+               "spomiędzy", "sponad", "spośród", "spoza", "sprzed", "u", "według", "wobec", "wokoło", "wokół",\
+               "wskutek", "wśród", "wzdłuż", "zamiast", "znad", "zza", "naprzeciw", "naprzeciwko"]
+pronoms_dat = ["dzięki", "ku", "przeciw", "przeciw", "wbrew"]
+pronoms_acc = ["przez"]
+pronoms_inst = []
+pronoms_loc = ["przy"]
+# Decline words inside list:
+
+for index in list_of_indexes:
+    print(index)
+    print(list_of_words[index])
+#genitif
+    if list_of_words[index - 1].lower() in pronoms_gen:
+        list_of_words[index] = DeleteTags(list_of_words[index])
+        list_of_words[index] = Decline_Noun(list_of_words[index], 'gen')
+#datif
+    elif list_of_words[index - 1].lower() in pronoms_dat:
+        list_of_words[index] = DeleteTags(list_of_words[index])
+        list_of_words[index] = Decline_Noun(list_of_words[index], 'dat')
+#accusatif
+    elif list_of_words[index - 1].lower() in pronoms_acc:
+        list_of_words[index] = DeleteTags(list_of_words[index])
+        list_of_words[index] = Decline_Noun(list_of_words[index], 'acc')
+#instrumental
+    elif list_of_words[index - 1].lower() in pronoms_inst:
+        list_of_words[index] = DeleteTags(list_of_words[index])
+        list_of_words[index] = Decline_Noun(list_of_words[index], 'inst')
+#locatif
+    elif list_of_words[index - 1].lower() in pronoms_loc:
+        list_of_words[index] = DeleteTags(list_of_words[index])
+        list_of_words[index] = Decline_Noun(list_of_words[index], 'loc')
+    else:
+        list_of_words[index] = DeleteTags(list_of_words[index])
+        list_of_words[index] = Decline_Noun(list_of_words[index], 'nom')
+
+print(list_of_words)
